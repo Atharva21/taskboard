20ee1dd90625f03ae4c32a54f2d468d74458d97c
Atharva21 <atharva.m.mulay@gmail.com>
2021-10-01 18:48:32 +0530

    creates reusable column component

--- taskboard-client/src/components/board/Column.jsx
+++ taskboard-client/src/components/board/Column.jsx
@@ -1,26 +1,11 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import TaskComponent from "../ui/TaskComponent";
 
 import styled from "styled-components";
 import Task from "./Task";
-
+import ColumnComponent from "../ui/ColumnComponent";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 
-const Container = styled.div`
-	background-color: ${(props) => (props.isDragging ? "#f2d338" : "inherit")};
-	margin: 8px;
-	border: 2px solid #f0c90a;
-	border-radius: 1.3em;
-	width: 220px;
-	text-align: center;
-	display: flex;
-	flex-direction: column;
-	overflow: hidden;
-`;
-const Title = styled.h3`
-	border-radius: 0.7em;
-	padding: 8px;
-`;
 const TaskList = styled.div`
 	padding: 8px;
 	transition: background-color 0.2s ease;
@@ -39,72 +24,25 @@ const Column = ({
 	onColumnEdit,
 	onTaskEdit,
 }) => {
-	const titleRef = useRef();
-
-	const [isEditing, setEditing] = useState(false);
-
-	const toggleEditable = () => {
-		setEditing((prev) => !prev);
-	};
-
-	const clickHandler = () => {
-		if (!isEditing) toggleEditable();
-		setTimeout(() => {
-			titleRef.current.focus();
-		}, 0);
-	};
-
-	const onBlurHandler = () => {
-		if (isEditing) toggleEditable();
-		setTimeout(() => {
-			titleRef.current.blur();
-		}, 0);
-
-		if (
-			titleRef.current.innerHTML &&
-			column.title !== titleRef.current.innerHTML &&
-			titleRef.current.innerHTML.length > 0
-		) {
-			const newTitle = titleRef.current.innerHTML;
-			titleRef.current.innerHTML = column.title;
-			onColumnEdit(column._id, newTitle);
-		}
-	};
-
 	const taskEditHandler = (newContent) => {
 		onTaskAdd(newContent, column._id);
+	};
+
+	const titleEditHandler = (newContent) => {
+		onColumnEdit(column._id, newContent);
 	};
 
 	return (
 		<Draggable draggableId={column._id} index={index}>
 			{(provided, snapshot) => (
-				<Container
-					{...provided.draggableProps}
-					ref={provided.innerRef}
+				<ColumnComponent
+					draggableProps={provided.draggableProps}
+					dragHandleProps={provided.dragHandleProps}
+					titleEditHandler={titleEditHandler}
+					innerRef={provided.innerRef}
+					title={column.title}
 					isDragging={snapshot.isDragging}
 				>
-					<Title
-						{...provided.dragHandleProps}
-						ref={titleRef}
-						onDoubleClick={clickHandler}
-						onBlur={onBlurHandler}
-						spellCheck="false"
-						contentEditable={isEditing ? "true" : "false"}
-						suppressContentEditableWarning={true}
-						style={
-							isEditing
-								? {
-										cursor: "text",
-										backgroundColor: "lightyellow",
-								  }
-								: {
-										cursor: "inherit",
-								  }
-						}
-						ref={titleRef}
-					>
-						{column.title}
-					</Title>
 					<Droppable droppableId={column._id} type="task">
 						{(provided, snapshot) => (
 							<TaskList
@@ -124,17 +62,15 @@ const Column = ({
 								})}
 								{provided.placeholder}
 								{!maxTasks && (
-									<div>
-										<TaskComponent
-											content=""
-											taskEditHandler={taskEditHandler}
-										></TaskComponent>
-									</div>
+									<TaskComponent
+										content=""
+										taskEditHandler={taskEditHandler}
+									></TaskComponent>
 								)}
 							</TaskList>
 						)}
 					</Droppable>
-				</Container>
+				</ColumnComponent>
 			)}
 		</Draggable>
 	);
--- taskboard-client/src/components/ui/ColumnComponent.jsx
+++ taskboard-client/src/components/ui/ColumnComponent.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+	background-color: ${(props) => (props.isDragging ? "#f2d338" : "inherit")};
+	margin: 8px;
+	border: 2px solid #f0c90a;
+	border-radius: 1.3em;
+	width: 220px;
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+`;
+const Title = styled.h3`
+	border-radius: 0.7em;
+	padding: 8px;
+
+	&:hover {
+		cursor: grab;
+	}
+`;
 
 const ColumnComponent = (props) => {
-	return <div></div>;
+	const titleRef = useRef();
+
+	const [isEditing, setEditing] = useState(false);
+
+	const toggleEditable = () => {
+		setEditing((prev) => !prev);
+	};
+
+	const clickHandler = () => {
+		if (!isEditing) toggleEditable();
+		setTimeout(() => {
+			titleRef.current.focus();
+		}, 0);
+	};
+
+	const onBlurHandler = () => {
+		if (isEditing) toggleEditable();
+		setTimeout(() => {
+			titleRef.current.blur();
+		}, 0);
+
+		if (
+			titleRef.current.innerHTML &&
+			props.title !== titleRef.current.innerHTML &&
+			titleRef.current.innerHTML.length > 0
+		) {
+			const newTitle = titleRef.current.innerHTML;
+			titleRef.current.innerHTML = props.title;
+			props.titleEditHandler(newTitle);
+		}
+	};
+
+	const keyDownHandler = (e) => {
+		if (e.key === "Enter") {
+			titleRef.current.blur();
+		}
+	};
+
+	return (
+		<Container
+			ref={props.innerRef}
+			{...props.draggableProps}
+			isDragging={props.isDragging}
+		>
+			<Title
+				{...props.dragHandleProps}
+				ref={titleRef}
+				onDoubleClick={clickHandler}
+				onBlur={onBlurHandler}
+				onKeyDown={keyDownHandler}
+				spellCheck="false"
+				contentEditable={isEditing ? "true" : "false"}
+				suppressContentEditableWarning={true}
+				style={
+					isEditing
+						? {
+								cursor: "text",
+								backgroundColor: "lightyellow",
+						  }
+						: {
+								cursor: "inherit",
+						  }
+				}
+				ref={titleRef}
+			>
+				{props.title}
+			</Title>
+			{props.children}
+		</Container>
+	);
 };
 
 export default ColumnComponent;
